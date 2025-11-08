b5c8a1c23cc3a13deb9658c63580785e783b1232
nojimage <nojimage@gmail.com>
2025-11-08 13:56:49 +0900

    設定値に const 構文を利用

--- build/bootstrap.php
+++ build/bootstrap.php
@@ -1,21 +1,28 @@
 <?php
 define('ROOT', dirname(__DIR__));
-define('TMP', ROOT . '/tmp');
-define('DOWNLOAD_PAGE', 'https://www.soumu.go.jp/denshijiti/code.html');
-define('DOWNLOAD_LINK_TEXT', 'Excelファイル');
-define('DOWNLOAD_FILE', TMP . '/cities.xls');
-define('CITY_SHEET_INDEX', 0);
-define('CITY_SHEET_START_ROW', 2);
-define('WARD_SHEET_INDEX', 1);
-define('WARD_SHEET_START_ROW', 2);
-define('EXPORT_JSON_OPT', JSON_HEX_TAG | JSON_HEX_AMP | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
-define('EXPORT_CITIES_JSON', ROOT . '/cities.json');
-define('EXPORT_PREF_JSON', ROOT . '/prefectures.json');
-define('EXPORT_WARDS_JSON', ROOT . '/wards.json');
-define('EXPORT_FULL_JSON', ROOT . '/index.json');
-define('TABLE_NAME', 'jp_local_gov_codes');
-define('EXPORT_MYSQL', ROOT . '/jp_local_gov_codes.mysql.sql');
-define('EXPORT_SQLITE', ROOT . '/jp_local_gov_codes.sqlite.sql');
+const TMP = ROOT . '/tmp';
+const DOWNLOAD_PAGE = 'https://www.soumu.go.jp/denshijiti/code.html';
+const DOWNLOAD_LINK_TEXT = 'Excelファイル';
+const DOWNLOAD_FILE = TMP . '/cities.xls';
+const CITY_SHEET_INDEX = 0;
+const CITY_SHEET_START_ROW = 2;
+const WARD_SHEET_INDEX = 1;
+const WARD_SHEET_START_ROW = 2;
+const EXPORT_JSON_OPT = JSON_HEX_TAG |
+    JSON_HEX_AMP |
+    JSON_HEX_APOS |
+    JSON_HEX_QUOT |
+    JSON_PRETTY_PRINT |
+    JSON_UNESCAPED_SLASHES |
+    JSON_UNESCAPED_UNICODE |
+    JSON_THROW_ON_ERROR;
+const EXPORT_CITIES_JSON = ROOT . '/cities.json';
+const EXPORT_PREF_JSON = ROOT . '/prefectures.json';
+const EXPORT_WARDS_JSON = ROOT . '/wards.json';
+const EXPORT_FULL_JSON = ROOT . '/index.json';
+const TABLE_NAME = 'jp_local_gov_codes';
+const EXPORT_MYSQL = ROOT . '/jp_local_gov_codes.mysql.sql';
+const EXPORT_SQLITE = ROOT . '/jp_local_gov_codes.sqlite.sql';
 
 require_once(ROOT . '/vendor/autoload.php');
 
